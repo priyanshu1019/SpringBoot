e4dc494ca099fda3a588d1166b6701b688a91e79
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-10-21 19:17:30 +0530

    introduction to springboot

---
+++ src/main/java/com/codingShuttle/prianshu/week1Introduction/Intoduction/to/springboot/IntoductionToSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.prianshu.week1Introduction.Intoduction.to.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntoductionToSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntoductionToSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingShuttle/prianshu/week1Introduction/Intoduction/to/springboot/IntoductionToSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.prianshu.week1Introduction.Intoduction.to.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntoductionToSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
